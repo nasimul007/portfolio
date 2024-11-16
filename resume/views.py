deb62b849cddb33e0692206d5cd412110b46a426
nasimul007 <nasimul.hasan.aiub@gmail.com>
2024-11-16 17:02:37 +0600

    home page design completed

--- resume/views.py
+++ resume/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request, "portfolio/home.html")
+
+def about(request):
+    return render (request, "portfolio/about.html")
